04436411b1c52695ddc2f6b138d2bda717013292
Adrie Buendia <adriebuendia@icloud.com>
2022-08-13 15:24:09 -0400

    setting up solar project

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*jslint browser:true */
+"use strict";
+
+function addMonths (elem) {
+let  annualUseKw = 0, dailyUseKw = 0, i = 0, x = 0;
+let months = document.getElementById(elem).getElementsByTagName('input');
+console.log(months);
+
+for (i = 0; i<months.length; i++){
+    x = Number(months[i].value);
+    annualUseKw += x;
+    } // end loop
+dailyUseKw = annualUseKw/365;
+console.log(dailyUseKw);
+
+
+} // end function
+
+
+
+addMonths('mpc');
