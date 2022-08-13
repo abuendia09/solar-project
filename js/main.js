7032a81c0cf61de419eb6130c72e5ab3370e01df
Adrie Buendia <adriebuendia@icloud.com>
2022-08-13 16:19:10 -0400

    solar calculations completed

--- js/main.js
+++ js/main.js
@@ -15,6 +15,73 @@ let months = document.getElementById(elem).getElementsByTagName('input');
 } // end function
 
 
+function sunHours() {
+let hrs;
+let theZone = document.forms.solarForm.zone.selectedIndex;
+theZone +=1;
+switch(theZone) {
+    case 1:
+        hrs = 6;
+        break;
+    case 2:
+        hrs = 5.5;
+        break;
+    case 3:
+        hrs = 5;
+        break;
+    case 4:
+        hrs = 4.5;
+        break;
+    case 5:
+        hrs = 4.2;
+        break;    
+    case 6:
+        hrs = 3.5;
+        break;
+    default:
+        hrs = 0;
+    } // end of switch
+return hrs;
+} // End function
 
-let dailyUseKw = addMonths('mpc');
-console.log(dailyUseKw);
+function calculatePanel () {
+    let userChoice = document.forms.solarForm.panel.selectedIndex;
+    let panelOptions = document.forms.solarForm.panel.options;
+    let power = panelOptions[userChoice].value;
+    let panelName = panelOptions[userChoice].text;
+    let x = [power, panelName];
+    return x;
+
+} // end of function
+
+
+
+
+
+function calculateSolar() {
+    let dailyUseKw = addMonths('mpc');
+    console.log(dailyUseKw);
+
+    let sunHoursPerDay = sunHours();
+    console.log(sunHoursPerDay);
+
+    let minKwNeeds = dailyUseKw/sunHoursPerDay;
+    console.log(minKwNeeds);
+
+    let realKwNeeds = minKwNeeds * 1.25;
+    console.log(realKwNeeds);
+
+    let realWattNeeds = realKwNeeds * 1000;
+    console.log(realWattNeeds);
+
+    let panelInfo = calculatePanel();
+    let panelOutput = panelInfo[0];
+    let panelChoice = panelInfo[1];
+    console.log(panelOutput);
+    console.log(panelChoice);
+
+    let panelsNeeded = Math.ceil(realWattNeeds / panelOutput);
+    console.log(panelsNeeded);
+
+
+} //end function
