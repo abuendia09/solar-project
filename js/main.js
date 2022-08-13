12c774589284fd6d54f1e13d0daeb57025cf1cc5
Adrie Buendia <adriebuendia@icloud.com>
2022-08-13 15:38:20 -0400

    add months function complete and shows daily usage of KW

--- js/main.js
+++ js/main.js
@@ -4,18 +4,17 @@
 function addMonths (elem) {
 let  annualUseKw = 0, dailyUseKw = 0, i = 0, x = 0;
 let months = document.getElementById(elem).getElementsByTagName('input');
-console.log(months);
 
-for (i = 0; i<months.length; i++){
-    x = Number(months[i].value);
-    annualUseKw += x;
+    for (i = 0; i<months.length; i++){
+        x = Number(months[i].value);
+        annualUseKw += x;
     } // end loop
-dailyUseKw = annualUseKw/365;
-console.log(dailyUseKw);
-
+    dailyUseKw = annualUseKw/365;
+    return dailyUseKw;
 
 } // end function
 
 
 
-addMonths('mpc');
+let dailyUseKw = addMonths('mpc');
+console.log(dailyUseKw);
